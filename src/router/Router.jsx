96f584a4161cdf5518ce438defbb42e40e96c6b5
habibmrizki <habibboboho@gmail.com>
2026-08-25 19:53:16 +0700

    feat(router): configure application routes and protected views

---
+++ src/router/Router.jsx
@@ -0,0 +1,154 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// Layouts
+import AuthLayout from "../layouts/auth/AuthLayout";
+import MainLayout from "../layouts/main/MainLayout";
+import DashboardLayout from "../layouts/admin/DashboardLayouts";
+
+// User  Pages
+import Explore from "../pages/Explore/Explore";
+import Login from "../pages/auth/login/Login";
+import Register from "../pages/auth/register/Register";
+import ForgotPassword from "../pages/forgot-password/ForgotPassword";
+import ResetPassword from "../pages/reset-passwod/ResetPassword";
+
+import Events from "../pages/events/Events";
+import EventDetail from "../pages/events-detail/EventDetail";
+import Communities from "../pages/communities/Communities";
+import CommunityDetail from "../pages/communities/CommunitiesDetaill";
+import Notification from "../pages/notification/Notification";
+import Profile from "../pages/profile/Profile";
+import ProtectedRoute from "../components/auth/ProtectedRoute";
+
+// Admin Dashboard Pages
+import DashboardOverview from "../pages/admin/dashboard/DashboardOverview";
+import DashboardUsers from "../pages/admin/dashboard/DashboardUsers";
+import DashboardEvents from "../pages/admin/dashboard/DashboardEvents";
+import DashboardCommunities from "../pages/admin/dashboard/DashboardCommunities";
+
+// Organizer Dashboard Pages
+import OrganizerDashboard from "../pages/Organizer/dashboard/Dashboard";
+import CreateEvent from "../pages/Organizer/createEdit/CreateEditEvent";
+
+// My Events
+import MyEvents from "../pages/my-events/MyEvents";
+// import Mnt from "../pages/Mnt";
+
+export default function MainRoutes() {
+  return (
+    <Routes>
+      <Route element={<AuthLayout />}>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Route>
+
+      {/* <Route path="/Mnt" element={<Mnt />} /> */}
+
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Navigate to="/explore" replace />} />
+        <Route path="/explore" element={<Explore />} />
+
+        {/* User Protected Routes */}
+        <Route
+          path="/events"
+          element={
+            <ProtectedRoute>
+              <Events />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/events/:id"
+          element={
+            <ProtectedRoute>
+              <EventDetail />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/communities"
+          element={
+            <ProtectedRoute>
+              <Communities />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/communities/:id"
+          element={
+            <ProtectedRoute>
+              <CommunityDetail />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/notifications"
+          element={
+            <ProtectedRoute>
+              <Notification />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/my-events"
+          element={<Navigate to="/my-events/upcoming" replace />}
+        />
+        <Route
+          path="/my-events/:tab"
+          element={
+            <ProtectedRoute>
+              <MyEvents />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Organizer Routes */}
+        <Route
+          path="/organizer"
+          element={
+            <ProtectedRoute>
+              <OrganizerDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/organizer/create-event"
+          element={
+            <ProtectedRoute>
+              <CreateEvent />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Admin Dashboard Routes  */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route
+            index
+            element={<Navigate to="/dashboard/overview" replace />}
+          />
+          <Route path="overview" element={<DashboardOverview />} />
+          <Route path="users" element={<DashboardUsers />} />
+          <Route path="events" element={<DashboardEvents />} />
+          <Route path="communities" element={<DashboardCommunities />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
+}
