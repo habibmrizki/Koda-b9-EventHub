b977ec3d190cdbd6b380131262899c1de55a0e66
habibmrizki <habibboboho@gmail.com>
2026-09-03 02:45:14 +0700

    refactor(auth): update ProtectedRoute to use useAuth hook and trigger auth modal

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -1,14 +1,20 @@
+import { useEffect, useRef } from "react";
 import { useLocation, Navigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { openAuthModal } from "../../redux/slices/authSlices/authSlice";
+import useAuth from "../../hooks/useAuth";
 
 export default function ProtectedRoute({ children }) {
-  const dispatch = useDispatch();
-  const currentUser = useSelector((state) => state.auth.currentUser);
+  const { currentUser, openAuthModal } = useAuth();
   const location = useLocation();
 
+  const wasLoggedIn = useRef(Boolean(currentUser));
+
+  useEffect(() => {
+    if (!currentUser && !wasLoggedIn.current) {
+      openAuthModal(location.pathname);
+    }
+  }, [currentUser, location.pathname, openAuthModal]);
+
   if (!currentUser) {
-    dispatch(openAuthModal(location.pathname));
     return <Navigate to="/explore" replace />;
   }
 
